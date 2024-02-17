23e4fd498c6af0b1b4c2434aaf8d3c6277017ba6
David <davidhe.3612@gmail.com>
2024-02-16 21:01:44 -0500

    ok

--- src/datab/students.js
+++ src/datab/students.js
@@ -1,10 +1,3 @@
-let students = [
-    {
-        firstName: "John",
-        familyName: "Smith",
-        DOB: "2000-01-01",
-        id: '0b3902d0-ff9b-4f6e-8bc9-3597c0a91a4f'
-    }
-];
+let students = [];
 
 module.exports = students;
