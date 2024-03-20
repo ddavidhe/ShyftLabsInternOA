9fb52e498c94e5228001bd57b68a9537ba914bc9
David <davidhe.3612@gmail.com>
2024-03-20 13:43:34 -0400

    idnno i forget

--- frontend/src/components/StudentsTable.js
+++ frontend/src/components/StudentsTable.js
@@ -4,6 +4,25 @@ import React, { useState, useEffect } from "react";
 const StudentsTable = ({ dataFetched }) => {
   const [students, setStudents] = useState([]); // Initialize students as an empty array
 
+  // async function deleteStudent(index) {
+  //   const studentToDelete = students[index];
+  //   console.log("Student to delete: ", studentToDelete.id);
+  //   try {
+  //     const response = await fetch(`http://localhost:3000/students/${studentToDelete.id}`, {
+  //       method: "DELETE",
+  //     });
+
+  //     if (!response.ok) {
+  //       throw new Error("Failed to delete student");
+  //     }
+
+  //     const updatedStudents = students.filter((student, i) => i !== index);
+  //     setStudents(updatedStudents);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
+
   useEffect(() => {
     // Fetch students when the component mounts
     const fetchStudents = async () => {
@@ -22,6 +41,7 @@ const StudentsTable = ({ dataFetched }) => {
           <th>First Name</th>
           <th>Family Name</th>
           <th>DOB</th>
+          {/* <th>Delete</th> */}
         </tr>
       </thead>
       <tbody>
@@ -30,6 +50,9 @@ const StudentsTable = ({ dataFetched }) => {
             <td>{student.firstName}</td>
             <td>{student.familyName}</td>
             <td>{student.DOB}</td>
+            {/* <td>
+              <button onClick={() => deleteStudent(index)}>Delete</button>
+            </td> */}
           </tr>
         ))}
       </tbody>
